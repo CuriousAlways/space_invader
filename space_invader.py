314d2c5292e257e31690b7ab1550c74ca60802b2
raza khan <khaninattitude@gmail.com>
2020-01-22 11:33:52 +0530

    added background color using fill method of surface object returned by pygame.display.set_mode()

--- space_invader.py
+++ space_invader.py
@@ -9,18 +9,24 @@ class Space_Invader :
 		pygame.init(); #initializes background setting of pygame
 		self.screen = pygame.display.set_mode((1200,800)) #creates the game window
 		pygame.display.set_caption('Space Invader')
+		self.bg_color = (200,200,200)
 
 	def run_game(self) :
 
 		while True:
 
-			for event in pygame.event.get():  #an event loop to recognise user action
+			#an event loop to recognise user action
+			for event in pygame.event.get():  
 				print(f'event : {event}')     #just a debug statement
 
-				if (event.type == pygame.QUIT) :  # identifying the event if game's close window button is pressed 
+				# identifying the event if game's close window button is pressed 
+				if (event.type == pygame.QUIT) :  
 					sys.exit()
 
-			pygame.display.flip()  #drawing the screen with updated game element states
+			#gives background color to game window by redrawing screen on every loop 
+			self.screen.fill(self.bg_color) 
+			#drawing the screen with updated game element states
+			pygame.display.flip()  
 
 
 
