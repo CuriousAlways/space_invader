e376742c9c5d2a7d22538843e7968f47fbf0d327
raza khan <khaninattitude@gmail.com>
2020-01-22 15:16:36 +0530

    improved space_invader module for better management

--- space_invader.py
+++ space_invader.py
@@ -18,21 +18,28 @@ class Space_Invader :
 	def run_game(self) :
 
 		while True:
+			self._check_event()
+			self._update_screen()  
 
-			#an event loop to recognise user action
+	def _check_event(self) :
+
+		#an event loop to recognise user action
 			for event in pygame.event.get():  
-				print(f'event : {event}')     #just a debug statement
-
+				print(f'event : {event}')     #just a debug statement 
 				# identifying the event if game's close window button is pressed 
 				if (event.type == pygame.QUIT) :  
 					sys.exit()
+
+	def _update_screen(self) :
+
+		#gives background color to game window by redrawing screen on every loop 
+		self.screen.fill(self.setting.bg_color)
+		#draws ship on screen
+		self.ship.draw_ship() 
+		#drawing the screen with updated game element states
+		pygame.display.flip()
 			
-			#gives background color to game window by redrawing screen on every loop 
-			self.screen.fill(self.setting.bg_color)
-			#draws ship on screen
-			self.ship.draw_ship() 
-			#drawing the screen with updated game element states
-			pygame.display.flip()  
+
 
 
 
