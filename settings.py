905b7f68689a9bb826775897a46712cebff905c6
raza khan <khaninattitude@gmail.com>
2020-01-22 11:49:12 +0530

    added Settings class in settings module

---
+++ settings.py
@@ -0,0 +1,8 @@
+
+
+class Settings :
+	'''class which contains game settings'''
+	def __init__(self) :
+		self.width = 1200
+		self.height = 800
+		self.bg_color =(200,200,200)
--- space_invader.py
+++ space_invader.py
@@ -1,15 +1,16 @@
 import sys
 import pygame
-
+from settings import Settings 
 
 class Space_Invader :
 	'''class to manage game resources and assets'''
 	def __init__(self) :
 
+		self.setting = Settings() 
+
 		pygame.init(); #initializes background setting of pygame
-		self.screen = pygame.display.set_mode((1200,800)) #creates the game window
+		self.screen = pygame.display.set_mode((self.setting.width,self.setting.height)) #creates the game window
 		pygame.display.set_caption('Space Invader')
-		self.bg_color = (200,200,200)
 
 	def run_game(self) :
 
@@ -24,7 +25,7 @@ class Space_Invader :
 					sys.exit()
 
 			#gives background color to game window by redrawing screen on every loop 
-			self.screen.fill(self.bg_color) 
+			self.screen.fill(self.setting.bg_color) 
 			#drawing the screen with updated game element states
 			pygame.display.flip()  
 
